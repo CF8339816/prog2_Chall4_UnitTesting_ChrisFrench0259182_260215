1f29e3568d7dd1f8d49d4d06fdc8a85a5c33d248
CF8339816 <chris_french_3dga@hotmail.com>
2026-02-15 21:26:07 -0400

    added player class.

---
+++ prog2_Chall4_UnitTesting_ChrisFrench0259182_260215/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2_Chall4_UnitTesting_ChrisFrench0259182_260215
+
+public class Player
+{
+    public int MaxHealth { get; private set; }
+    public int CurrentHealth { get; private set; }
+
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Player(int maxHealth, int startX, int startY)
+    {
+        MaxHealth = maxHealth;
+        CurrentHealth = maxHealth;
+        X = startX;
+        Y = startY;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth < 0)
+        {
+            CurrentHealth = 0;
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        CurrentHealth += amount;
+        if (CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+    }
+
+    public void Move(int deltaX, int deltaY)
+    {
+        X += deltaX;
+        Y += deltaY;
+    }
+}
